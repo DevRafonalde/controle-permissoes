85f945732bca7f81f9891e0263b2469b9651f3d1
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-07-21 17:09:04 -0300

    Delete src directory

--- src/main/java/br/com/dev1risjc/ControlePermissoes/models/repositories/PermissaoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.dev1risjc.ControlePermissoes.models.repositories;
-
-import br.com.dev1risjc.ControlePermissoes.models.entities.Permissao;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PermissaoRepository extends CrudRepository<Permissao, Integer> {
-}
--- src/main/java/br/com/dev1risjc/ControlePermissoes/models/repositories/SistemasRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.dev1risjc.ControlePermissoes.models.repositories;
-
-import br.com.dev1risjc.ControlePermissoes.models.entities.Sistema;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SistemasRepository extends CrudRepository<Sistema, Integer> {
-}

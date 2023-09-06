bd63a6d2d4ae6d449328883f3dc2bfad9b5a9f18
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2023-09-06 07:57:48 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/dev1risjc/ControlePermissoes/models/repositories/UsuarioRepository.java
+++ src/main/java/br/com/dev1risjc/ControlePermissoes/models/repositories/UsuarioRepository.java
@@ -4,11 +4,9 @@ import br.com.dev1risjc.ControlePermissoes.models.entities.orm.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
     @Query("Select x from Usuario x where x.NomeAmigavel like %:NomeAmigavel%")
-    List<Usuario> findByNomeAmigavel(String NomeAmigavel);
+    Usuario findByNomeAmigavel(String NomeAmigavel);
 
     Usuario findByNomeUser(String NomeUser);
 }
